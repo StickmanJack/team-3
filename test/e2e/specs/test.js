c130da113f01cd733b5adf028fa91f158a5ce319
Wisdom <rjw3695@live.missouristate.edu>
2017-04-21 13:54:18 -0500

    This is the Nightwatch e2e test

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -1,19 +1,16 @@
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
+// Goes in $project_folder/test/e2e/specs/
 
 module.exports = {
-  'default e2e tests': function (browser) {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
-    const devServer = browser.globals.devServerURL
-
-    browser
-      .url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end()
-  }
+	"Test home page": function(browser) {
+		const devServer = browser.globals.devServerURL;
+		browser
+			.url(devServer)
+			.waitForElementVisible('body', 2000)
+			.assert.containsText('h1', 'Good Afternoon Chad!')
+			.assert.containsText('.row-eq-height .tall-col .well h2', 'Current Weather')
+			.assert.containsText('.row-eq-height .tall-col .row .col-sm-12 .well h2', 'Quote of the Day:')
+			.end();
+	}
 }
