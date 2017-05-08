e2e524ec0e8a72d5aa8fb99996feadb670641fb4
Seth <sethtucker88@gmail.com>
2017-05-08 17:34:09 -0500

    Added funtionality to 'TodayDetails.vue' and 'SimpleForcast.vue' for getting the user defined zipcode to pass to the API. Information displayed by 'TodayDetails.vue' updates correctly. Added node module to convert degrees to direction for wind direction.

--- src/components/TodayDetails.vue
+++ src/components/TodayDetails.vue
@@ -15,19 +15,19 @@
             <h4>Weather:</h4>
             <p>{{description}}</p>
             <h4>Temperature:</h4>
-            <p>{{localTemp}} F</p>
+            <p>{{localTemp}}</p>
             <h4>High</h4>
-            <p>{{highTemp}} F</p>
+            <p>{{highTemp}}</p>
             <h4>Low</h4>
-            <p>{{lowTemp}} F</p>
+            <p>{{lowTemp}}</p>
             <h4>Humidity:</h4>
-            <p>{{humidity}}%</p>
+            <p>{{humidity}}</p>
             <h4>Wind:</h4>
-            <p>{{speed}} mph {{direction}}</p>
+            <p>{{speed}} {{direction}}</p>
             <h4>Sunrise</h4>
-            <p>{{sunrise}} am</p>
+            <p>{{sunrise}}</p>
             <h4>Sunset</h4>
-            <p>{{sunset}} pm</p>
+            <p>{{sunset}}</p>
           </div>
         </div>
       <div class="col-sm-1"></div>
@@ -36,18 +36,85 @@
 </template>
 
 <script>
+// Calls a node module that converts degrees to cardinal directions
+var d2d = require('degrees-to-direction')
 export default {
   data () {
     return {
-      description: 'Light Rain',
-      localTemp: '75',
-      highTemp: '77',
-      lowTemp: '68',
-      humidity: '86',
-      speed: '5.1',
-      direction: 'NE',
-      sunrise: '6:45',
-      sunset: '7:01'
+      description: '',
+      localTemp: '',
+      highTemp: '',
+      lowTemp: '',
+      humidity: '',
+      speed: '',
+      direction: '',
+      sunrise: '',
+      sunset: '',
+      zip: this.getCookie('zip')
+    }
+  },
+  mounted: function () {
+    // Automatically fetch weather when returning to this page from another
+    if (this.zip !== '') {
+      this.getCurrentConditions()
+    }
+  },
+  methods: {
+    // Access weather API and get current weather info
+    getCurrentConditions: function () {
+      this.description = 'Loading...'
+      this.localTemp = 'Loading...'
+      this.highTemp = 'Loading...'
+      this.lowTemp = 'Loading...'
+      this.humidity = 'Loading...'
+      this.speed = 'Loading...'
+      this.sunrise = 'Loading...'
+      this.sunset = 'Loading...'
+      let url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + this.zip + ',us' +
+                '&units=imperial' + // Unit default is Kelvin
+                '&appid=0c2cf2d862aed4851bd89af90698bc92'
+      fetch(url).then(function (response) {
+        return response.json()
+      }).then(function (json) {
+        // Set current temperature and conditions
+        this.description = json.weather[0].main
+        this.localTemp = json.main.temp + String.fromCharCode(176) + ' F'
+        this.highTemp = json.main.temp_max + String.fromCharCode(176) + ' F'
+        this.lowTemp = json.main.temp_min + String.fromCharCode(176) + ' F'
+        this.humidity = json.main.humidity + '%'
+        this.speed = json.wind.speed + ' mph'
+        this.direction = d2d(json.wind.deg)
+        this.sunrise = this.getTime(json.sys.sunrise) + ' am'
+        this.sunset = this.getTime(json.sys.sunset) + ' pm'
+        // Updates weather icon according to current conditions
+        document.getElementById('weather-icon').src = this.getImgUrl(this.conditions + '.png')
+      }.bind(this)).catch(function (reason) {
+        console.log(reason)
+      })
+    },
+    getCookie: function (cname) {
+      var name = cname + '='
+      var decodedCookie = decodeURIComponent(document.cookie)
+      var ca = decodedCookie.split(';')
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i]
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1)
+        }
+        if (c.indexOf(name) === 0) {
+          return c.substring(name.length, c.length)
+        }
+      }
+      return ''
+    },
+    getTime: function (timestamp) {
+      var date = new Date(timestamp * 1000)
+      // Converts military hours into standard hours
+      var hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours()
+      // Values less than 10 will have a '0' inserted infront
+      var minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+      var formattedTime = hours + ':' + minutes
+      return formattedTime
     }
   }
 }
